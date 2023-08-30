904632de2956e43494e59351ec78a8d54a74c1ce
James <joshua.james918@gmail.com>
2023-08-29 20:34:52 -0500

    more code to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -5,9 +5,43 @@ const notes =('express').router();
 const { v4: uuidv4} = require('uuid');
 
 // functions for reading and writing to the Json file
-const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils')
+const { readFromFile, writeToFile, readAndAppend } = require('..fsUtils/helpers/fsUtils');
+const { json } = require('body-parser');
 
 // API route to GET Route for retreiving all the notes
  notes.get('/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
  });
+
+//  get route for specific note
+  notes.get('/notes',(req, res) => {
+    const id = req.params.id;
+     readFromFile('./db/db.json')
+     .then((data) => json.parse(data))
+     .then((json) => {
+        const result = json.filter((note) => note.id === id);
+        return result.length > 0
+        ? res.json(result)
+        : res.json('no id found');
+     });
+  });
+
+//   delete route for specific note
+  notes.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+     readFromFile('./db/db.json')
+     .then((data) => json.parse(data))
+     .then((json) => {
+
+        // new array
+        const result = json.filter((note) => note.id !== id);
+
+    // array writes to file
+    writeToFile('./db/db.json', resut);
+
+    // delete request
+    res.json(`Item ${id} deleted`)
+
+  });
+});
+
